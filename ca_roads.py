ce543501491204603ccd3fb3bd5116fb0dd15e9d
Paul Warren <paul.warren@ogsystems.com>
2015-06-13 04:58:33 -0400

    model.py creates 256x256 node network and turns it into an undirected graph, need to add imaging to see what i'm doing, then can work on migration also

---
+++ ca_roads.py
@@ -0,0 +1,56 @@
+from snap import *
+
+def printGStats(s, Graph):
+    '''
+    Print graph statistics
+    '''
+
+    print "graph %s, nodes %d, edges %d, empty %s" % (
+        s, Graph.GetNodes(), Graph.GetEdges(),
+        "yes" if Graph.Empty() else "no")
+
+def DefaultConstructor():
+    '''
+    Test the default constructor
+    '''
+
+    Graph = TNEANet.New()
+    PrintGStats("DefaultConstructor:Graph", Graph)
+
+g = TNEANet.New()
+g.AddFltAttrN("immune")
+g.AddFltAttrN("susceptible")
+g.AddFltAttrN("recovered")
+g.AddFltAttrN("infected")
+g.AddFltAttrN("dead")
+g.AddFltAttrN("total")
+
+printGStats("CA ROADS", g)
+
+for i in range(0, 1971281):
+	g.AddNode(i)
+
+
+printGStats("CA ROADS", g)
+
+# with open("/Users/paul.warren/Documents/ebola/data/roadNet-CA.txt", "r") as f:
+#     for line in f.readlines():
+#         parts = line.split()
+#         g.AddEdge(int(parts[0]), int(parts[1]))
+
+printGStats("CA ROADS", g)
+
+# get all the nodes
+# NCount = 0
+# NI = Graph.BegNI()
+# while NI < Graph.EndNI():
+#     NCount += 1
+#     NI.Next()
+
+# print("done with shit")
+
+
+
+
+
+

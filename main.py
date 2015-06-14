af06986a68c07e9a3d60ee27fe0ec2091df83c61
Paul Warren <paul.warren@ogsystems.com>
2015-06-13 20:38:18 -0400

    committing dan's changes

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ import model
 immuneRate = .1	 # percentage of people immune: 
 fatalityRate = .5 # chance of dying vs recovering (becoming immune): 
 averageDistance = 1000 # kilometers per day an average person in Africa can travel: 
-transmissionRate = .5 # chance you'll infect a person you interact with:
 
 # --- START MODEL
-print "Reading data..."
-m = model.Model("data/node_train.dat", immuneRate, fatalityRate, averageDistance, transmissionRate)
-
-for i in range(0, 1):
-	print "Turn %d" % i
-	m.turn()
+m = model.Model("data/node.dat", immuneRate, fatalityRate, averageDistance)
+print "finished"
+#m.viz()

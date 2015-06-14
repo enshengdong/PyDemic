02d113fec3aecc3a24cbe2c130eea7e851a14cc6
rbmj <rbmj@verizon.net>
2015-06-14 17:31:40 -0400

    Add kill count

---
+++ getKillCount.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+
+from osgeo import gdal, osr
+import numpy
+import math
+import sys
+
+geoTIFF = gdal.Open(sys.argv[1])
+rows = geoTIFF.RasterYSize
+cols = geoTIFF.RasterXSize
+band = geoTIFF.GetRasterBand(1)
+emptyval = band.GetNoDataValue()
+data = band.ReadAsArray(0, 0, cols, rows).astype(numpy.float)
+accumulate = 0L
+for row in data:
+    for x in row:
+        if x == emptyval:
+            continue
+        accumulate = accumulate + long(x)
+print(accumulate)
+

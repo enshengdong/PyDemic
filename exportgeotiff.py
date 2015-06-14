187394cf0af9600158ccab6286976a58f8054915
rbmj <rbmj@verizon.net>
2015-06-14 00:33:32 -0400

    Change Tiff Arch.

--- exportgeotiff.py
+++
@@ -1,6 +0,0 @@
-import numpy
-from popdata import *
-
-arr = numpy.load('node.npy')
-exportGeoTIFF('pop_out.tif', arr, roi, float(numpy.finfo(numpy.float32).min))
-
---
+++ getROItiff.py
@@ -0,0 +1,10 @@
+import numpy
+from popdata import *
+
+emptyval = float(numpy.finfo(numpy.float32).min)
+
+arr = numpy.load('node.npy')
+
+(rasterbase, geo) = nidDataToArrayCB(arr, roi, lambda _: float(0.0), emptyval)
+exportGeoTIFFRaster('base_roi.tif', rasterbase, geo, emptyval)
+

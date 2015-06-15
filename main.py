04885c1d1f6450fea1d5e7008745ffa210153371
Paul Warren <paul.warren@ogsystems.com>
2015-06-15 14:34:31 -0400

    refactoring: - made node separate file changed "%d" % var -> {}.format(), more pythonic added *.pyc to .gitignore changed averageDistance to maxDistance (just name change) made it load nodes.dat instead of node.dat (just name change)

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import model
 import time
 
 # --- EVOLUTION FACTORS
-immuneRate = .1  # percentage of people immune
+immunityRate = .1  # percentage of people immune
 fatalityRate = .6  # chance of dying vs recovering (becoming immune)
-averageDistance = 1000  # kilometers per day an average person in Africa can travel:
+maxDistance = 1000  # kilometers per day an average person in Africa can travel:
 transmissionRate = .5  # chance you'll infect a given person you come in contact with
 
 # --- START MODEL
-print "Starting model..."
-m = model.Model("data/node.dat", immuneRate, fatalityRate, averageDistance, transmissionRate)
-print "Finished model. Starting turn..."
-# m.viz()
-for i in range(0, 100):
+print "Creating model..."
+m = model.Model("data/nodes.dat", immunityRate, fatalityRate, maxDistance, transmissionRate)
+print "Starting turns..."
+
+for i in range(0, 2):
     start = time.time()
     m.turn()
     if i % 1 == 0:
         m.dump(i)
-    print "Finished turn %d in %d seconds" % (i, time.time() - start)
+    print "Finished turn {0} in {1:.2f} seconds.".format(i, time.time() - start)
--- model.py
+++ model.py
@@ -1,110 +1,24 @@
 import numpy as np
 import time
-
-class Node():
-    """ 
-    Helper Node class. Represents a 1km x 1km tile. Takes in Node ID, latitude, longitude, population, the probability of contagious people 
-    going North when traveling, the ID of the node to the North, the probability of contagious people going East when traveling, the ID of 
-    the node to the East, the probability of contagious people going South when traveling, the ID of the node to the South, the probability 
-    of contagious people going West when traveling, the ID of the node to the North, the probability of contagious people staying instead of
-    traveling, and the percentage of the overall population who are immune to the disease. 
-
-    Has nid,latitude,longitude,population,probNorth,northNID,probEast,eastNID,probSouth,southNID,probWest,westNID,probStay,immuneRate,immune,
-    susceptible,incubation1,incubation2,contagiousA,contagiousB, and dead fields. Immune, susceptible, ... fields all represent the total number
-    of people in that 1km x 1km region who are in that stage of the disease. ContagiousB is used to store contagious people who have all traveled
-    before; when calculating the number of contagious people in a given node, you should sum contagiousA and contagious B.
-    """
-    def __init__(self,nid,latitude,longitude,population,probNorth,northNID,probEast,eastNID,probSouth,southNID,probWest,westNID,probStay,immuneRate):
-        """
-        Initializes node with given information.
-        """
-        self.nid = int(nid)
-        self.latitude = latitude
-        self.longitude = longitude
-        self.probNorth = probNorth
-        self.northNID = int(northNID)
-        self.probEast = probEast
-        self.eastNID = int(eastNID)
-        self.probSouth = probSouth
-        self.southNID = int(southNID)
-        self.probWest = probWest
-        self.westNID = int(westNID)
-        self.probStay = probStay
-
-        self.immune = np.random.poisson(int(population*immuneRate))
-        self.susceptible = int(round(population - self.immune))
-        self.incubation1 = 0
-        self.incubation2 = 0
-        self.contagiousA = 0
-        self.contagiousB = 0  
-        self.dead = 0
-
-    def infect(self):
-        """
-        Subroutine that nodes run when a contagious person passes through to determine how many people that contagious person infects.
-        Automatically updates information.
-        """
-        y = 2 # people interacted with * transmission probability
-        I = np.random.poisson(y)
-        if I <= self.susceptible:
-            self.susceptible -= I
-            self.incubation1 += I
-        else:
-            self.incubation1 += self.susceptible
-            self.susceptible = 0
-
-    def getLatLong(self):
-        """
-        Subroutine that returns latitude and longitude in a convenient format for our parser.
-        """
-        return [self.longitude,self.latitude,1.0]
-
-    def getIncubating(self):
-        """
-        Subroutine that returns the number of people in the incubutation stage of the disease in a convenient format for our parser.
-        """
-        return self.incubation1 + self.incubation2
-
-    def getContagious(self):
-        """
-        Subroutine that returns the number of people in the contagious stage of the disease in a convenient format for our parser.
-        """
-        return self.contagiousA + self.contagiousB
-
-    def __str__(self):
-        """
-        to_string subroutine that combines all of the interesting data (latitude, longitude, immune, susceptible, incubation1, 
-        incubation2, contagiousA, contagiousB, dead) in each node in a convenient format for our parser.
-        """
-        out = ""
-        out += str(self.latitude) + ":"
-        out += str(self.longitude) + ":"
-        out += str(self.immune) + ":"
-        out += str(self.susceptible) + ":"
-        out += str(self.incubation1) + ":"
-        out += str(self.incubation2) + ":"
-        out += str(self.contagiousA) + ":"
-        out += str(self.contagiousB) + ":"
-        out += str(self.dead)
-        return out
-
+import node
+import os
 
 class Model():
     """ 
     The Model classes represents our overall network-based model. It takes in a filename of all data necessary to initialize each node
-    (sans immuneRate, which is passed into the model during creation). It also takes in evolutionary factors immuneRate (the percentage 
-    of the population immune to the disease), fatalityRate (how likely it is a sick person will die or recover), averageDistance 
+    (sans immunityRate, which is passed into the model during creation). It also takes in evolutionary factors immunityRate (the percentage 
+    of the population immune to the disease), fatalityRate (how likely it is a sick person will die or recover), maxDistance 
     (distance a contagious traveler could be expected to cover in our given time step), and transmissionRate (percentage of people a 
     contagious person will infect out of the total people they'll come in contact with).
     """
-    def __init__(self,filename,immuneRate,fatalityRate,averageDistance,transmissionRate):
+    def __init__(self,filename,immunityRate,fatalityRate,maxDistance,transmissionRate):
         """
         Initializes model with given information.
         """
         self.filename = filename
-        self.immuneRate = immuneRate
+        self.immunityRate = immunityRate
         self.fatalityRate = fatalityRate
-        self.averageDistance = averageDistance
+        self.maxDistance = maxDistance
 
         self.nodes = self._readFile()  # np.array of _Node objects
 
@@ -112,24 +26,21 @@ class Model():
         """
         Helper subroutine that reads the given filename and initializes all the nodes.
         """
-        with open(self.filename,"r") as f:
+        with open(self.filename, "r") as f:
             numNodes = int(f.next().strip())
-            nodes = np.empty(numNodes,dtype=np.object)
+            nodes = np.empty(numNodes, dtype=np.object)
             count = 0
             start = time.time()
             for line in f:
                 inputs = [float(f) for f in line[:-1].split(':')]
-                inputs.append(self.immuneRate)
-                nodes[count] = Node(*(inputs))
+                inputs.append(self.immunityRate)
+                nodes[count] = node.Node(*(inputs))
                 count += 1
-                # if count > 1000000:
-                #    break
-            print("Graph creation time: " + str(time.time() - start))
-            print("Created %d nodes" % (count))
+            print("{0} nodes loaded into a model in {1:.2f} seconds.".format(count, time.time() - start))
             nodes[10].incubation1 += 30
             for i in range(0,5):
                 nodes[np.random.randint(0,numNodes)].incubation1 += 5
-        return nodes[:count]
+        return nodes[:count] # TO-DO: REMOVE
 
     def turn(self):
         """
@@ -139,9 +50,8 @@ class Model():
         self.travel()
         self.resetContagious()
 
-
     def resetContagious(self):
-        for i in range(0,self.nodes.shape[0] - 1):
+        for i in range(0, len(self.nodes)):
             node = self.nodes[i]
             node.contagiousA += node.contagiousB
             node.contagiousB = 0
@@ -158,7 +68,7 @@ class Model():
         have one state transition a turn. ContagiousB is the set of contagious people who have already
         moved or done a state transition and so can't move for the rest of the timestep.
         """
-        for i in range(0,self.nodes.shape[0] - 1):
+        for i in range(0,len(self.nodes)):
             node = self.nodes[i]
             # print("node.contagiousB = " + str(node.contagiousB))
             node.contagiousB += node.incubation2
@@ -181,7 +91,7 @@ class Model():
         person travels to, and uses the given fatality rate to whether
         the contagious person dies or recovers.
         """
-        for i in range(0,self.nodes.shape[0] - 1):
+        for i in range(0,len(self.nodes)):
             for j in range(0,self.nodes[i].contagiousA):
                 curNode = self.nodes[i]
                 for step in range(0,2050):
@@ -204,17 +114,20 @@ class Model():
                     curNode.immune += 1
                 self.nodes[i].contagiousA -= 1
 
-    def dump(self,frame):
+    def dump(self, frame):
         """
         Subroutine that prints out the data in the graph in a format convenient for our parser.
         """
         dead = 0
-        with open("graphLong.%d.dmp" % (frame),"w") as out:
+        if not os.path.exists("data/dumps"):
+            os.mkdir("data/dumps")
+
+        with open("data/dumps/graphLong.{}.dmp".format(frame), "w") as out:
             for node in self.nodes:
                 if node is not None:
                     dead += node.dead
                     out.write(str(node)+"\n")
-        print "%d Dead" % dead
+        print "{} Dead".format(dead)
 
 
 

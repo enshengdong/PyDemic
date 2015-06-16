4220a18b4ffed4afbae0be3e7079a307b7c95b11
dbs4261 <dbs4261@rit.edu>
2015-06-16 09:05:03 -0400

    Updated gitignore Process now has a single convert function that takes a glob of files.

--- process.py
+++ process.py
@@ -1,16 +1,19 @@
 import sys
 import numpy
 from popdata import *
+import time
+import glob
 
 def dataToArray(data, roi, attr, emptyval):
     (xcoords, ycoords) = getCoords(roi)
-    arr = numpy.full((len(ycoords+1), len(xcoords+1)), emptyval, dtype=numpy.float32)
+    arr = numpy.full((len(ycoords), len(xcoords)), emptyval, dtype=numpy.float32)
     print arr.shape
     geotrans = createGeoTrans(roi)
     geotrans_m = getGeoTransM(geotrans).I
     for node in data:
         (x, y) = getLocPixel(geotrans_m, node[1], node[0])
-        arr[y, x] = node[attr]
+        if (x < arr.shape[1]) and (y < arr.shape[0]):
+            arr[y, x] = node[attr]
     return (arr, geotrans)
 
 def convert(files):
@@ -23,9 +26,14 @@ def convert(files):
                 node[4] = node[4] + node[5]
                 node[6] = node[6] + node[7]
             for field in params:
+                start = time.time()
                 (raster, trans) = dataToArray(data, roi, field[1], empty)
-                exportGeoTIFFRaster(fname + '.' + field[0] + '.tif', raster, trans, empty)
+                exportGeoTIFFRaster(fname[:-4] + '.' + field[0] + '.tif', raster, trans, empty)
+                print "%d seconds" % (time.time() - start)
 
 if __name__ == '__main__':
-    files = sys.argv[1:]
+    #files = sys.argv[1:]
+    files = ["graphLong.6.dmp"]
+    files = glob.glob("*.dmp")
+    print files
     convert(files)
